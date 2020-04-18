e36c270ac83297b03d02ded4b274ded7e9d13176
Nigel Metheringham <nigelm@cpan.org>
2020-04-18 21:16:07 +0100

    Fixed build script

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -21,7 +21,7 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+(cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)")
 
 echo "Pushing to github"
 git push --all
